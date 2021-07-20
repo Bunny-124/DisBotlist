3268864294d9bd27d8ab19c02f18d0c286027dbc
Xo <76423014+Bunny-124@users.noreply.github.com>
2021-07-20 08:14:54 +0530

    Update bump.js

--- src/servers/commands/bump.js
+++ src/servers/commands/bump.js
@@ -5,7 +5,7 @@ const serverData = require("../../database/models/servers/server.js");
 exports.run = async (client, message, args) => {
 	let findServer = await serverData.findOne({ id: message.guild.id });
 	if(!findServer) return message.channel.send(
-		"This server was not found in our list.\nAdd your server: https://disbotlist.xyz/server/add"
+		"This server was not found in our list.\nAdd your server: https://topiclist.uve.repl.co/server/add"
 	);
 	let cooldown = 1800000;
   	let lastDaily = findServer.bump;
@@ -42,7 +42,7 @@ exports.run = async (client, message, args) => {
     let web = new MessageButton()
     .setLabel("Visit server page")
     .setStyle("url")
-    .setURL("https://disbotlist.xyz/server/"+message.guild.id)
+    .setURL("https://topiclist.uve.repl.co/server"+message.guild.id)
 
     const incorrectButton = new Discord.MessageEmbed()
 	.setTitle("Wrong button selected.")
@@ -107,7 +107,7 @@ exports.help = {
 function msgError(msg, { channel }) {
     channel.send(new Discord.MessageEmbed()
     .setAuthor(global.clientSL.user.username,global.clientSL.user.avatarURL())
-    .setFooter('disbotlist.xyz/servers')
+    .setFooter('topiclist.uve.repl.co/servers')
     .setDescription(msg)
     .setColor("RED")
     )
